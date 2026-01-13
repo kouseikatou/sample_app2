275ae68ee64145027f278e35fe6f015f771e7f9a
kouse <kouse1113sp@gmail.com>
2026-01-13 18:15:24 +0900

    Exec:rails generate controller StaticPages home help

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,4 +1,6 @@
 class HelloController < ApplicationController
   def index
+    # app/views/コントローラー名/アクション名.html.erb
+    # app/views/hello_cotroller/index.html.erb
   end
 end
---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,12 @@
+class StaticPagesController < ApplicationController
+  def home
+    # render ....
+    # => app/views/static_pages/home.html.erb
+  end
+
+   # => app/views/コントローラー名/アクション名.html.erb
+  def help
+     # => app/views/static_pages/help.html.erb
+  end
+  end
+end
