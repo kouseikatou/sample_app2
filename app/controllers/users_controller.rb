c96b3962fe3d6c9c338d31b02f5083113d26c426
kouse <kouse1113sp@gmail.com>
2026-01-24 11:17:53 +0900

    finish user ingnup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,47 @@
 class UsersController < ApplicationController
+
+  def index
+
+  end
+
+    # GET /users/:id
+  def show
+    # @user = User.first
+    @user = User.find(params[:id]) 
+
+    # => app/views/users/show.html.erb
+  end
+
+    # GET /users/new
   def new
     # app/views/users/new.html.erb　こちらにアクセス
+    @user = User.new
+  end
+
+    #POST /users
+  def create
+    # params => user.save => if ... else ... end
+    # params[user[:name]] => 'foober'
+    # params[user[:email]] =>'foober@gmail.com'
+    @user = User.new(user_params)
+    if @user.save
+      # => Success
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+      # redirect_to user_path(@user)    => GET /users/:id
+      # redirect_to user_path(@user.id) => GET /users/2
+
+
+    else
+      # => user/new
+      render 'new',status: :unprocessable_entity
+    end    
+  end
+
+  # praivateの中のミニヘルパーはファイル内ででは呼び出すことが可能。
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
